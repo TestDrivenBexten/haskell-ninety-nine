914129e211497eb46e8eb06b8a57a101d4e16c5f
Skyler Bexten <skyler.bexten@gmail.com>
2020-03-03 20:06:06 -0700

    Write function to determine prime numbers

--- src/ArithmeticNinetyNine.hs
+++ src/ArithmeticNinetyNine.hs
@@ -3,5 +3,9 @@ module ArithmeticNinetyNine
         isPrime
     ) where
 
+import ListNinetyNine    
+
 isPrime :: Int -> Bool
-isPrime x = True
+isPrime x
+    | x < 2 = False
+    | otherwise = not (any (\y -> (x `mod` y) == 0) (range 2 (x - 1)))
--- test/ArithmeticSpec.hs
+++ test/ArithmeticSpec.hs
@@ -8,3 +8,15 @@ spec = do
     describe "When checking whether a number is prime" $ do
         it "Zero is not prime" $ do
             isPrime 0 `shouldBe` False
+
+        it "One is not prime" $ do
+            isPrime 1 `shouldBe` False
+
+        it "Two is prime" $ do
+            isPrime 2 `shouldBe` True
+        
+        it "Twelve is not prime" $ do
+            isPrime 12 `shouldBe` False
+            
+        it "Thirteen is prime" $ do
+            isPrime 13 `shouldBe` True
