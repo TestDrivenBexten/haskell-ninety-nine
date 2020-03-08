16f895b6f2e55ba4fdcd1b9c2138bf13833cd9d1
Skyler Bexten <skyler.bexten@gmail.com>
2020-03-08 14:54:35 -0600

    Solve OR function

--- src/LogicNinetyNine.hs
+++ src/LogicNinetyNine.hs
@@ -1,6 +1,10 @@
 module LogicNinetyNine
-    ( myAnd
+    ( myAnd,
+        myOr
     ) where
 
 myAnd :: Bool -> Bool -> Bool
 myAnd a b = a && b
+
+myOr :: Bool -> Bool -> Bool
+myOr a b = a || b
--- test/LogicSpec.hs
+++ test/LogicSpec.hs
@@ -14,3 +14,13 @@ spec = do
             myAnd False True `shouldBe` False
         it "F F Should return F" $ do
             myAnd False False `shouldBe` False
+            
+    describe "Given the OR operator" $ do
+        it "T T Should return T" $ do
+            myOr True True `shouldBe` True
+        it "T F Should return T" $ do
+            myOr True False `shouldBe` True
+        it "F T Should return T" $ do
+            myOr False True `shouldBe` True
+        it "F F Should return F" $ do
+            myOr False False `shouldBe` False
