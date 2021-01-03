d5a99722958b4f7d007d118906b944d0522493a4
Skyler Bexten <skyler.bexten@gmail.com>
2021-01-03 16:46:12 -0700

    Failing test for tree total

--- src/Huffman.hs
+++ src/Huffman.hs
@@ -1,4 +1,20 @@
-module Huffman (huffman)  where
+module Huffman (
+    huffman,
+    buildHuffmanTree,
+    Tree(Leaf,Branch),
+    treeTotal
+)  where
+
+type CharFrequency = (Char,Int)
+data Tree = Leaf { value :: CharFrequency } |
+    Branch { leftChild :: Tree, rightChild :: Tree }
 
 huffman :: [(Char,Int)] -> [(Char,String)]
 huffman xs = [('a',"01")]
+
+buildHuffmanTree :: [CharFrequency] -> Tree
+buildHuffmanTree xs = Leaf ('a',1)
+
+treeTotal :: Tree -> Int
+treeTotal (Leaf x) = 5
+treeTotal (Branch left right) = 5
--- test/HuffmanSpec.hs
+++ test/HuffmanSpec.hs
@@ -9,3 +9,9 @@ spec = do
         it "Sample with six character frequencies" $ do
             huffman [('a',45),('b',13),('c',12),('d',16),('e',9),('f',5)]
                 `shouldBe` [('a',"0"),('b',"101"),('c',"100"),('d',"111"),('e',"1101"),('f',"1100")]
+
+    describe "Frequency List to Tree" $ do
+        describe "Given frequency d = 10 and c = 5" $ do
+            let huffmanTree = buildHuffmanTree [('d',10),('c',5)]
+            it "Then the parent node should have a value of 15" $ do
+                treeTotal huffmanTree `shouldBe` 15
