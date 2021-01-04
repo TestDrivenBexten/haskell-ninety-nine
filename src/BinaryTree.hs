5621f68c9de065b6419fcca48dfc0870845c0def
Skyler Bexten <skyler.bexten@gmail.com>
2021-01-03 20:26:45 -0700

    Hardcode values for node counts of 1 and 2

--- src/BinaryTree.hs
+++ src/BinaryTree.hs
@@ -7,4 +7,6 @@ data Tree a = Empty | Branch a (Tree a) (Tree a)
     deriving (Eq, Show)
 
 cBal :: Int -> [Tree Char]
-cBal nodeCount = [Branch 'x' Empty Empty]
+cBal 1 = [Branch 'x' Empty Empty]
+cBal 2 = [Branch 'x' (Branch 'x' Empty Empty) Empty,
+            Branch 'x' Empty (Branch 'x' Empty Empty)]
--- test/BinaryTreeSpec.hs
+++ test/BinaryTreeSpec.hs
@@ -6,6 +6,9 @@ import BinaryTree
 spec :: Spec
 spec = do
     describe "P55. Construct balanced binary trees" $ do
+        it "Balanced tree with 1 node should have one permutation" $ do
+            cBal 1 `shouldBe` [Branch 'x' Empty Empty]
+
         it "Balanced tree with 2 nodes should have two permutations" $ do
             cBal 2 `shouldBe` [Branch 'x' (Branch 'x' Empty Empty) Empty,
                 Branch 'x' Empty (Branch 'x' Empty Empty)]
