a328ac4aa490b8a1acb8d15442b4486d98b6a7f6
Skyler Bexten <skyler.bexten@gmail.com>
2021-01-03 19:03:29 -0700

    Function to combine huffman trees, incomplete

--- src/Huffman.hs
+++ src/Huffman.hs
@@ -6,15 +6,19 @@ module Huffman (
 )  where
 
 type CharFrequency = (Char,Int)
-data Tree = Leaf CharFrequency |
-    Branch { leftChild :: Tree, rightChild :: Tree }
+data Tree = Empty | Leaf CharFrequency |
+    Branch { leftChild :: Tree, rightChild :: Tree } deriving (Eq, Show)
 
 huffman :: [(Char,Int)] -> [(Char,String)]
 huffman xs = [('a',"01")]
 
 buildHuffmanTree :: [CharFrequency] -> Tree
-buildHuffmanTree [x,y] = Branch (Leaf x) (Leaf y)
+buildHuffmanTree xs = combineHuffmanTree [ Leaf x | x <- xs ]
 
 treeTotal :: Tree -> Int
 treeTotal (Leaf x) = snd x
 treeTotal (Branch left right) = treeTotal left + treeTotal right
+
+combineHuffmanTree :: [Tree] -> Tree
+combineHuffmanTree [x] = x
+combineHuffmanTree xs = combineHuffmanTree (tail xs)
--- test/HuffmanSpec.hs
+++ test/HuffmanSpec.hs
@@ -12,6 +12,9 @@ spec = do
                 [('a',"0"),('b',"101"),('c',"100"),('d',"111"),('e',"1101"),('f',"1100")]
 
     describe "Frequency List to Tree" $ do
+        it "A single frequency should build a leaf node" $ do
+            buildHuffmanTree [('e',52)] `shouldBe` Leaf ('e',52)
+
         describe "Given frequency d = 10 and c = 5" $ do
             let huffmanTree = buildHuffmanTree [('d',10),('c',5)]
             it "Then the parent node should have a value of 15" $ do
