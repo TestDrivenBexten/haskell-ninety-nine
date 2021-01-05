1bfbe678b9bd9f58c6215314b246a60fb8585787
Skyler Bexten <skyler.bexten@gmail.com>
2021-01-04 20:06:45 -0700

    Use case for three nodes

--- src/BinaryTree.hs
+++ src/BinaryTree.hs
@@ -11,3 +11,4 @@ xNode = Branch 'x' Empty Empty
 cBal :: Int -> [Tree Char]
 cBal 1 = [xNode]
 cBal 2 = [Branch 'x' xNode Empty, Branch 'x' Empty xNode]
+cBal 3 = [Branch 'x' xNode xNode]
--- test/BinaryTreeSpec.hs
+++ test/BinaryTreeSpec.hs
@@ -12,3 +12,7 @@ spec = do
         it "Balanced tree with 2 nodes should have two permutations" $ do
             cBal 2 `shouldBe` [Branch 'x' (Branch 'x' Empty Empty) Empty,
                 Branch 'x' Empty (Branch 'x' Empty Empty)]
+
+        it "Balanced tree with 3 nodes should have one permutation" $ do
+            cBal 3 `shouldBe` [Branch 'x' (Branch 'x' Empty Empty) 
+                                            (Branch 'x' Empty Empty)]
